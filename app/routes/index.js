c291052f61c27a478b3af1f3bb79c4dd5d150cc5
Chris-1101 <chrismb.noreply@gmail.com>
2019-03-14 22:12:15 +0000

    :wrench: Setup home page routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,20 @@
+const { Question, Answer, AnswerType } = require('../db/config')
 const router = require('express').Router();
 
 // GET Home Page
-router.get('/', (req, res) => res.render('index', { appName: "Magic 8-Ball" }));
+router.get('/', (req, res) => {
+  Question.findAll({ include: [{ model: Answer, include: [AnswerType] }] })
+  .then(questions => {
+    res.render('index', { appName: "Magic 8-Ball", questions: questions });
+  });
+});
+
+// POST New Question
+router.post('/', (req, res) => {
+  Question.create({
+    text: req.body.question,
+    answerId: Math.floor(Math.random() * 20) + 1
+  }).then(() => res.redirect('/'));
+});
 
 module.exports = router;
