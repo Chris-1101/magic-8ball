e250a906071e128df8b6753a4f7287842fe9344a
Chris-1101 <chrismb.noreply@gmail.com>
2019-03-16 17:29:17 +0000

    :wrench: Add bootstrap support

--- app/views/layout.jsx
+++ app/views/layout.jsx
@@ -13,9 +13,10 @@ class PageLayout extends React.Component
           <meta charSet="UTF-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
-          <title>{ this.props.appName } - { this.props.title }</title>
-          <link rel="stylesheet" type="text/css" href="stylesheets/style.css" />
+          <title>Magic 8-Ball - { this.props.title }</title>
+          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
           <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" />
+          <link rel="stylesheet" type="text/css" href="stylesheets/style.css" />
         </head>
         <body>
           <NavBar />
