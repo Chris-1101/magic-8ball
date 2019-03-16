881f54f25652fa9992a399aea9ba49a94e96fe38
Chris-1101 <chrismb.noreply@gmail.com>
2019-03-16 01:23:05 +0000

    :sparkles: Add support for LESS stylesheets

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const port = 3000;
 
 // Other Dependencies
 const bodyParser = require('body-parser');
+const less = require('less-middleware');
 const jsx = require('express-react-views');
 const path = require('path');
 
@@ -15,6 +16,7 @@ const statsRouter = require('./app/routes/stats');
 // Config
 app.use(bodyParser.json()); // Parse POST Requests
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(less(path.join(__dirname, 'app/public'))); // LESS Support
 app.use(express.static('app/public')); // Static Resources
 app.set('views', path.join(__dirname, 'app/views')); // Views
 app.set('view engine', 'jsx'); // View Engine
