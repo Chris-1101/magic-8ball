f28e2eba7c4b7251dad88193cf983073a2725e92
Chris-1101 <chrismb.noreply@gmail.com>
2019-03-16 20:12:33 +0100

    :sparkles: Add 'About' nav link

--- app/views/_nav.jsx
+++ app/views/_nav.jsx
@@ -5,13 +5,16 @@ class NavBar extends React.Component
 {
   render()
   {
+    const readme = 'https://github.com/Chris-1101/magic-8ball/blob/master/README.md';
+
     return (
       <div className="flex spbe">
         <img src="images/peak_logo.png" id="peak-logo" alt="PEAK" />
         <div className="flex spbe">
           <ul className="inline-list">
-            <NavItem icon="far fa-question-circle" path="#" label="Questions" />
-            <NavItem icon="fas fa-chart-bar" path="#" label="Statistics" />
+            <NavItem icon="far fa-question-circle" path="/" opens="_self" label="Questions" />
+            <NavItem icon="fas fa-chart-bar" path="/stats" opens="_self" label="Statistics" />
+            <NavItem icon="far fa-file-alt" path={ readme } opens="_blank" label="About" />
           </ul>
           <i id="menu-logo" className="fas fa-bars"></i>
         </div>
@@ -28,7 +31,7 @@ class NavItem extends React.Component
     return(
       <li>
         <i className={ this.props.icon }></i>
-        <a href={ this.props.path }>{ this.props.label }</a>
+        <a href={ this.props.path } target={ this.props.opens }>{ this.props.label }</a>
       </li>
     );
   }
